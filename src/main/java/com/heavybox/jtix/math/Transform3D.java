b022170c32ee63269ced2cef73e047434d8e7082
Eilan Laken <eilanlaken@gmail.com>
2026-02-27 10:24:25 +0200

    m

--- src/main/java/com/heavybox/jtix/math/Transform3D.java
+++ src/main/java/com/heavybox/jtix/math/Transform3D.java
@@ -5,14 +5,14 @@ import org.jetbrains.annotations.NotNull;
 // TODO: test
 public class Transform3D {
 
-    public Vector3    position = new Vector3(0,0,0);
-    public Quaternion rotation = new Quaternion();
-    public Vector3    scale    = new Vector3(1,1,1);
+    public Vector3    translation = new Vector3(0,0,0);
+    public Quaternion rotation    = new Quaternion();
+    public Vector3    scale       = new Vector3(1,1,1);
 
     public Transform3D() {}
 
     public Transform3D(final Transform3D other) {
-        this.position = new Vector3(other.position);
+        this.translation = new Vector3(other.translation);
         this.rotation = new Quaternion(other.rotation);
         this.scale = new Vector3(other.scale);
     }
@@ -20,7 +20,7 @@ public class Transform3D {
     /** Sets the quaternion to an identity Quaternion
      * @return this quaternion for chaining */
     public Transform3D idt() {
-        this.position.set(0,0,0);
+        this.translation.set(0,0,0);
         this.rotation.idt();
         this.scale.set(1,1,1);
         return this;
@@ -29,7 +29,7 @@ public class Transform3D {
     public Vector3 transform(Vector3 v) {
         v.scl(scale);
         v.rot(rotation);
-        v.add(position);
+        v.add(translation);
         return v;
     }
 
@@ -40,12 +40,12 @@ public class Transform3D {
 
     // TODO: test
     public Matrix4x4 toMatrix(@NotNull Matrix4x4 mat) {
-        mat.setToTranslationRotationScaling(position, rotation, scale);
+        mat.setToTranslationRotationScaling(translation, rotation, scale);
         return mat;
     }
 
     @Override
     public String toString() {
-        return "position: " + position + "\n" + "rotation: " + rotation + "\n" + "scale: " + scale;
+        return "position: " + translation + "\n" + "rotation: " + rotation + "\n" + "scale: " + scale;
     }
 }
